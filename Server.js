fd27357762d5fd1b6fda6f9220d8d792fb5cc11f
sbgh <33097880+sbgh@users.noreply.github.com>
2019-07-21 10:51:31 -0600

    Delete Server.js

--- Server.js
+++
@@ -1,8 +0,0 @@
-//Server Listener requires ./src/app.js
-
-var http = require('http');
-var app = require("./src/app");
-
-server = http.createServer(app);
-server.listen('80');
-console.log("Express server listening on port 80");
