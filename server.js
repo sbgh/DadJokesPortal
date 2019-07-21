69a20beb191dc1d1922727d991522c174d5d626e
sbgh <33097880+sbgh@users.noreply.github.com>
2019-07-21 10:53:09 -0600

    del Server.js create serve.js

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by scott on 2019-07-21.
+ * Server Listener requires ./src/app.js
+ */
+
+var http = require('http');
+var app = require("./src/app");
+
+server = http.createServer(app);
+server.listen('80');
+console.log("Express server listening on port 80");
